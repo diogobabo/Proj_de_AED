a8978d2aa8bf7975effbc9c72f8eeb6667436e72
diogobabo <diogo.babo@gmail.com>
2021-12-03 11:22:28 +0000

    cena

---
+++ cmake-build-release/CMakeFiles/Proj_de_AED.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proj_de_AED.dir/main.cpp.obj"
+  "Proj_de_AED.exe"
+  "Proj_de_AED.exe.manifest"
+  "Proj_de_AED.pdb"
+  "libProj_de_AED.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proj_de_AED.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
